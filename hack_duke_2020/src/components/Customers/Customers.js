ae1ea9436cf4461185a24355c1222a3fe123b360
Kyang0433 <kyang0433@gmail.com>
2020-12-05 20:31:05 -0500

    Debugging fuse $2

--- hack_duke_2020/src/components/Customers/Customers.js
+++ hack_duke_2020/src/components/Customers/Customers.js
@@ -1,18 +1,89 @@
 import React, { Component } from "react";
 import { Container, Nav, Button, InputGroup, FormControl } from "react-bootstrap";
+import Fuse from 'fuse.js';
 import "./Customers.css";
+import firebase from "../../Firebase/firebase.js";
 
 class Customers extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      itemList: [],
+      query: "",
+      businessData: {}
+    };
   }
   
+  componentDidMount = () => {
+    /**
+     * Get business data in JSON form
+     */
+    let db = firebase.db;
+    let businessRef = db.ref('Stores');
+    businessRef.on('value', snapshot => {
+      this.setState({
+        businessData: snapshot.val()
+      });
+    })
+  }
   handleSearch = () => {
+    let items = [];
+    let businessData = this.state.businessData;
+    for (const store in businessData) {
+      let inventory = businessData[store]["Inventory"];
+      for (const item in inventory) {
+        if (item in items) {
+          /**
+           * Generates unique item key in cases where different
+           * stores have items with the same name
+           */
+          let i = 1;
+          let itemKey = item + i;
+          while (itemKey in items) {
+            i++;
+            itemKey = item + i;
+          }
+          /**
+           * Parsing the data to work with fuse.js
+           */
+          let itemObject = {
+            "itemName" : itemKey,
+            ...inventory[item]
+          }
+          items.push(itemObject);
+        } else {
+          let itemObject = {
+            "itemName" : item,
+            ...inventory[item]
+          }
+          items.push(itemObject);
+        }
+      }
+    }
+
+    console.log(items);
+
+
+
+    const options = {
+      threshold: 0.4,
+      keys: "itemName"
+    }
+    // const fuse = new Fuse(items, options);
+    // const pattern = this.state.query;
+    // let list = fuse.search(pattern);
+    // this.setState({
+    //   itemList: list
+    // })
     
+  }
+  
+  handleSearchChange = (event) => {
+    this.setState({query: event.target.value});
   }
 
   render() {
+    console.log(this.state.businessData);
     return (
       <Container className="Customers">
         <h1>Customers Page</h1>
@@ -24,6 +95,8 @@ class Customers extends Component {
             <FormControl
               aria-label="Default"
               aria-describedby="inputGroup-sizing-default"
+              value={this.state.query}
+              onChange={this.handleSearchChange}
             />
             <InputGroup.Append>
               <Button onClick={this.handleSearch}> 
@@ -31,6 +104,10 @@ class Customers extends Component {
               </Button>
             </InputGroup.Append>
           </InputGroup>
+
+          {this.state.itemList.map((x)=>{
+            return x;
+          })}
         </div>
       </Container>
     );
